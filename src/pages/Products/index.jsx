db05f6069601b644b5588d1e1494509ef44bc7db
czhu-24 <blue.ink.stains@gmail.com>
2023-09-28 00:48:08 -0700

    finished up i think

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,34 +1,48 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+// to generate unique ids for each product
 
 const Products = () => {
+	const [products, setProducts] = useState([]);
+	const [productName, setProductName] = useState("");
+	const [productDescription, setProductDescription] = useState("");
+	const [productPrice, setProductPrice] = useState(0);
 
-	const [product, setProduct] = useState(
-		{
-			name: "",
-			description: "",
-			price: 0
+	useEffect(() => {
+		if (products.length > 0){
+			// only log if products is NOT an empty array
+			console.log("A new product has been added!");
+			console.log("The new products is ", products);
 		}
-	);
-
-	const handleName = () => {
-
-	}
+	}, [products]);
 
 	const addProduct = (e) => {
 		e.preventDefault();
+		const newProduct = {
+			id: uuidv4(), 
+			name: productName, 
+			description: productDescription, 
+			price: productPrice
+		};
+		setProducts([...products, newProduct]);
+		setProductName("");
+		setProductDescription("");
+		setProductPrice(0);
 	}
 	return (
 		<>
 			<h1>Products</h1>
 			<form onSubmit={addProduct}>
 				<label htmlFor="name">Name:</label>
-				<input onChange={handleName} type="text" name="name" placeholder="Enter name" />
+				<input value={productName} 
+				onChange={(e) => setProductName(e.target.value)} type="text" name="name" placeholder="Enter product name" />
 				<label htmlFor="description">Description:</label>
-				<input type="text" name="description" placeholder="Enter description" />
-
+				<input value={productDescription} 
+				onChange={(e) => setProductDescription(e.target.value)} type="text" name="description" placeholder="Enter description" />
 				<label htmlFor="price">Price:</label>
-				<input type="number" name="price" placeholder="Enter price" step="0.1" />
-				<button></button>
+				<input value={productPrice} 
+				onChange={(e) => setProductPrice(e.target.value)} type="number" name="price" placeholder="Enter price" step="0.1" />
+				<button>Submit</button>
 			</form>
 		</>
 	)
